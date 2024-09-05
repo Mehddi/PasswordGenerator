76a7ec8f9c8558dd746cc6fb57c91ff4322a3f14
Mehddi <beloual.mehdi@yahoo.fr>
2024-09-05 23:40:28 +0200

    Premier nettoyage

--- PasswordGen.py
+++ PasswordGen.py
@@ -30,12 +30,3 @@ for i in range (carNumber):
         password += charSpeciaux
 
 print(password)
-
-
-''' random.randint(a, b) : entier aléatoire entre a et b (inclus).
-random.uniform(a, b) : flottant aléatoire entre a et b.
-random.random() : flottant entre 0.0 et 1.0.
-random.choice(seq) : élément aléatoire d'une séquence.
-random.shuffle(seq) : mélange une séquence.
-random.sample(seq, k) : tire un échantillon aléatoire de k éléments.
-random.seed(x) : fixe la graine pour contrôler la reproductibilité.'''
